50d4020259bf275f84defff3dbd85305410b3b24
seonghanna <150984422+seonghanna@users.noreply.github.com>
2025-11-11 14:29:11 +0900

    fix2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState ,useEffect, useRef} from 'react';
+import React, { useState ,useEffect} from 'react';
 import './App.css';
 import * as XLSX from "xlsx";
 import { saveAs } from "file-saver";
